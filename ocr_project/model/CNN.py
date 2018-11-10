fe54c2963c3592593ab1a6b6153dd38e50eaefcf
Kakoedlinnoeslovo <roma-degtayrev@mail.ru>
2018-11-10 11:59:13 +0300

    add cnn

--- ocr_project/model/CNN.py
+++ ocr_project/model/CNN.py
@@ -6,11 +6,13 @@ import numpy as np
 import tensorflow as tf
 import logging
 Logger()
-from model.CNN_utils import ConvRelu, max_2x2pooling
+from model.CNN_utils import ConvRelu, max_2x2pooling, ConvReluBN, max_2x1pool, dropout
 
 class CNN:
-    def __init__(self):
-        pass
+    def __init__(self, input_tensor, is_training):
+        self.model = None
+        self._build_network(input_tensor, is_training)
+
 
     def _build_network(self, input_tensor, is_training = False):
         logging.info("Start building CNN")
@@ -18,13 +20,47 @@ class CNN:
         net = tf.transpose(input_tensor, perm = [0, 2, 3, 1])
         net = tf.add(net, (-128.0))
         net = tf.multiply(net, (1/128.0))
+
         net = ConvRelu(net, 64, (3, 3), "conv_1")
-        net = ma
+        net = max_2x2pooling(net, name = "conv_pool1")
+
+        logging.info("net tensor shape: {}".format(net.get_shape()))
+
+        net = ConvRelu(net, 128, (3, 3), "conv_2")
+        net = max_2x2pooling(net, name="conv_pool2")
+
+        logging.info("net tensor shape: {}".format(net.get_shape()))
+
+        net = ConvReluBN(net, 256, (3,3), 'conv3', is_training)
+        net = ConvRelu(net, 256, (3,3), 'conv4')
+        net = max_2x1pool(net, "conv_pool3")
+
+        logging.info("net tensor shape: {}".format(net.get_shape()))
+
+        net = ConvReluBN(net, 512, (3, 3), 'conv5', is_training, padding_type="VALID")
+        net = ConvRelu(net, 512, (3, 3), 'conv6')
+        net = max_2x1pool(net, "conv_pool4")
+
+        logging.info("net tensor shape: {}".format(net.get_shape()))
 
 
+        net = ConvReluBN(net, 512, (2,2), 'conv_conv7', is_training)
+        net = dropout(net, is_training = is_training)
+
+        print("CNN outdim before squeeze: {}".format(net.get_shape())) #1x32x100 -> 24x512
+
+        net = tf.squeeze(net, axis = 1)
+
+        print("CNN outdim: {}".format(net.get_shape()))
+
+        self.model = net
+
+
+    def get_output(self):
+        return self.model
 
 
 if __name__ == "__main__":
-    cnn = CNN()
-    img_data = tf.placeholder(dtype = tf.float32, shape = (None, 1, 32, None), name = "input_data")
-    cnn._build_network(img_data)
+    img_data = tf.placeholder(dtype=tf.float32, shape=(None, 1, 32, 32), name="input_data")
+    cnn = CNN(img_data, is_training=True)
+    x = cnn.get_output()
--- ocr_project/model/CNN_utils.py
+++ ocr_project/model/CNN_utils.py
@@ -3,9 +3,12 @@ import math
 
 
 def var_random(name, shape,
-               filters_in,
-               filters_out,
                regularizable = False):
+
+    assert isinstance(shape, tuple), "shape should be tuple"
+
+    filters_in = shape[2]
+    filters_out = shape[3]
 
     variation = 2/(filters_in + filters_out)
     dev = math.sqrt(variation)
@@ -23,12 +26,11 @@ def ConvRelu(input, num_filters, filter_size, name):
     num_filters_from = input.get_shape().as_list()[3]
     with tf.variable_scope(name):
         conv_W = var_random("W", tuple(filter_size) + (num_filters_from, num_filters),
-                            filters_in = num_filters_from,
-                            filters_out = num_filters, regularizable=True)
+                            regularizable=True)
 
-        conv = tf.nn.conv2d(input = input, filter=conv_W, strides=(1,1,1,1), padding= "SAME")
+        after_conv = tf.nn.conv2d(input = input, filter=conv_W, strides=(1,1,1,1), padding= "SAME")
 
-        return tf.nn.relu(conv)
+        return tf.nn.relu(after_conv)
 
 
 def max_2x2pooling(incoming, name):
@@ -36,5 +38,28 @@ def max_2x2pooling(incoming, name):
         return tf.nn.max_pool(incoming, ksize = (1,2,2,1), strides=(1,2,2,1), padding = "VALID")
 
 
+def batch_norm(incoming, is_training):
+    return tf.contrib.layers.batch_norm(incoming,
+                                        is_training = is_training,
+                                        scale = True,
+                                        decay = 0.99)
+
+def ConvReluBN(incoming, num_filters, filter_size, name, is_training, padding_type = "SAME"):
+    num_filters_from = incoming.get_shape().as_list()[3]
+    with tf.variable_scope(name):
+        #shape var_random, for example: 3,3, 128, 256
+        conv_W = var_random(name = "W", shape = tuple(filter_size) + (num_filters_from, num_filters),
+                            regularizable=True)
+        after_conv = tf.nn.conv2d(input=incoming, filter=conv_W,
+                                  strides=(1,1,1,1), padding=padding_type)
+        after_bn = batch_norm(after_conv, is_training = is_training)
+        return tf.nn.relu(after_bn)
 
 
+def max_2x1pool(incoming, name):
+    with tf.variable_scope(name):
+        return tf.nn.max_pool(incoming, ksize = (1,2,1,1), strides = (1,2,1,1), padding="VALID")
+
+
+def dropout(incoming, is_training, keep_prob = 0.5):
+    return tf.contrib.layers.dropout(incoming, keep_prob = keep_prob, is_training = is_training)
